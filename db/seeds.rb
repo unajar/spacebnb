29bde17e09cef8f4676db7bf5cb78dd88d6608c9
benoit leboucher <ben.lebou@gmail.com>
2021-11-16 16:53:29 -0500

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-Planet.create (adress: "238251474028 Roswell crater", "A nice 122000 rooms in the middle of the crater with a lovely view on the erupting volcano")
+Planet.create (adress: "238251474028 Roswell crater", description: "A nice 122000 rooms in the middle of the crater with a lovely view on the erupting volcano", name: "Venus", price: "547 374 374$")
+Planet.create (adress: "173671728748 Allien montain", description: "a land of 3 billion square km to play real star wars in the garden", name: "Neptun", price: "299 999 999$")
+Planet.create (adress: "1 Mercure", description: "Rent the planet just for you, intimist week-en relax to enjoy the warm from the sun", name: "Mercure", price: "1 000 000 000$")
+Planet.create (adress: "The moon", description: "Come to see the flag raised in 1969", name: "Moon", price: "547 374 374$")
