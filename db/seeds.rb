9c8be8642dab29d9c2da8b1fe711ea4711e5acf6
benoit leboucher <ben.lebou@gmail.com>
2021-11-16 16:39:29 -0500

    Seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Planet.create (adress: "238251474028 Roswell crater", "A nice 122000 rooms in the middle of the crater with a lovely view on the erupting volcano")
