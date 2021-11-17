693dea6f377577ec22036c17c9dcaab3893b0ee5
Alaskar Talibov <aleskert@yahoo.fr>
2021-11-17 12:11:42 -0500

    Review of create-edit-destroy for planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -14,11 +14,24 @@ class PlanetsController < ApplicationController
   end
 
   def edit
+    @planet = Planet.find(params[:id])
   end
 
   def update
+    @planet = Planet.find(params[:id])
+    @planet.update(params[:planet])
+    redirect_to planet_path(@planet)
   end
 
   def destroy
+    @planet = Planet.find(params[:id])
+    @planet.destroy
+    redirect_to planet_path(@planet)
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :description, :address, :price, :user)
   end
 end
