52e84cde4435fe6a0d9cf8c7cd725c1b4fb55ae8
Alaskar Talibov <aleskert@yahoo.fr>
2021-11-17 15:47:18 -0500

    Reservations added to planet

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,29 @@
 class ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+    @planet = Planet.find(params[:planet_id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.planet = Planet.find(params[:planet_id])
+    @reservation.confirmed = false
+    if @reservation.save
+      redirect_to planets_path(@planet)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
