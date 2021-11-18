10eb315c6dbe493b733ddd62a34eb88ef7dab4ef
felix leveille <fil.leveille@hotmail.com>
2021-11-18 16:01:21 -0500

    created banch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_165704) do
+ActiveRecord::Schema.define(version: 2021_11_18_172141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
