3b1276d65c6b8389a172f2ecb6ab94e47cb69746
benoit leboucher <ben.lebou@gmail.com>
2021-11-18 13:23:34 -0500

    Add photo to create menu

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,6 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  has_one_attached :photo
   monetize :price_cents
 
   validates :address, :description, :name, :price, presence: true
