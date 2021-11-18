9fefd6e575f6af9457e8435298ff90b46948ca43
umberto najar <najar.umberto@gmail.com>
2021-11-18 16:50:11 -0500

    made some small changes on layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @planets = Planet.first(4)
+    @planets = Planet.all.reject do |planet|
+      current_user == planet.user
+    end.first(4)
   end
 end
